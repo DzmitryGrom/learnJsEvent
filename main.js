383557872ce4353b5d6fcccb3ffa5c7ffee28a1e
dzmitryshaliaheika <shelegeykodmitriy@gmail.com>
2018-07-21 14:07:45 +0300

    add lesson 14

---
+++ main.js
@@ -0,0 +1,30 @@
+showMenu(menu, 100, 10);
+
+function showMenu(element, x, y) {
+  let classList = element.classList,
+      style = element.style,
+      hideClass = 'hide',
+      pxUnit = 'px';
+
+  style.left = x + pxUnit;
+  style.top = y + pxUnit;
+
+  classList.remove(hideClass);
+
+  element.addEventListener('click', () => {
+    classList.add(hideClass);
+},
+  {
+    once: true
+  }
+)};
+
+document.addEventListener('contextmenu', function (e) {
+  event.preventDefault();
+
+  let x = event.clientX,
+      y = event.clientY;
+
+  showMenu(menu, x, y);
+
+}, false);
